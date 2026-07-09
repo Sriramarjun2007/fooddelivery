b6c6272890c9068d7cd6fc24ab9d57f766d32e31
Sriram Arjun <sriramarjun2007@gmail.com>
2026-07-09 09:23:30 +0530

    app.jsx error

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import WelcomPage from "./components/WelcomPage";
+import Welcompage from "./components/Welcompage";
 import Page2 from "./components/Page2";
 import Page3 from "./components/Page3";
 import Page4 from "./components/Page4";
@@ -8,7 +8,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<WelcomPage />} />
+        <Route path="/welcompage" element={<WelcomPage />} />
         <Route path="/page2" element={<Page2 />} />
         <Route path="/page3" element={<Page3/>}/>
         <Route path="/page4" element={<Page4/>}/>
